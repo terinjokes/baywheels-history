f7099f780d048d72f05d337870f9a24ea87383fd
Terin Stock <terinjokes@gmail.com>
2022-08-07 15:17:56 -0700

    chore: add script and action

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i oil -p oil
+
+... curl -Ss --fail https://gbfs.baywheels.com/gbfs/en/station_status.json -H"User-Agent: https://terinstock.com"
+    | jq -S '.data.stations |= sort_by(.station_id)'
+    > station_status.json
+    ;
+
+... curl -Ss --fail https://gbfs.baywheels.com/gbfs/en/station_information.json -H"User-Agent: https://terinstock.com"
+    | jq -S '.data.stations |= sort_by(.external_id)'
+    > station_information.json
+    ;
+
+... curl -Ss --fail https://gbfs.baywheels.com/gbfs/en/free_bike_status.json -H"User-Agent: https://terinstock.com"
+    | jq -S '.data.bikes |= sort_by(.bikes_id)'
+    > free_bike_status.json
+    ;
+
+... curl -Ss --fail https://gbfs.baywheels.com/gbfs/en/system_alerts.json -H"User-Agent: https://terinstock.com"
+    | jq -S '.data.alerts |= sort_by(.alerts_id)'
+    > system_alerts.json
+    ;
+
+... curl -Ss --fail https://gbfs.baywheels.com/gbfs/en/system_information.json -H"User-Agent: https://terinstock.com"
+    | jq -S
+    > system_information.json
+    ;
+
+git config user.name "GitHub Actions"
+git config user.email "actions@users.noreply.github.com"
+git commit -am "Latest data: $(date --iso-8601=seconds --utc)"
+git push
